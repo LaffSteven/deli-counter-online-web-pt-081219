c480f910367fa1a0a1e958b5a3401373527e44d6
Steven Laff <steve.laff@gmail.com>
2019-08-19 16:50:53 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,22 @@
-# Write your code here.
+
+katz_deli = []
+
+def line(katz_deli)
+  if katz_deli.empty? != true
+    katz_deli
+  else
+    "There is nobody waiting to be served!"
+end
+
+def take_a_number(katz_deli, name)
+  katz_deli << name
+end
+
+def now_serving
+  if katz_deli.empty? != true
+    puts "Currently serving #{katz_deli[0]}"
+    katz_deli.shift
+  else
+    "There is nobody waiting to be served!"
+  end
+end
