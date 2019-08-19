76dac6d6e005335cb904a142578554a608499009
Steven Laff <steve.laff@gmail.com>
2019-08-19 17:56:50 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,14 +2,16 @@
 katz_deli = []
 
 def line(katz_deli)
+  the_line = "The line is currently: "
   if katz_deli.empty? == true
     puts "The line is currently empty."
   else
-    line = "The line is currently"
+    
     katz_deli.each do |waiting_customer|
-      line << 
+      the_line << "#{katz_deli.index(waiting_customer) + 1}. #{waiting_customer} "
     end
-  end  
+  end
+  the_line
 end
 
 def take_a_number(katz_deli, new_customer)
